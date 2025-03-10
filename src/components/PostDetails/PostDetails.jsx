07f1137f78e50dbc90c2a496bbba2b9e1b1e10f4
Juwel-771 <juwelah99@gmail.com>
2025-03-11 02:29:04 +0600

    navigate hook set up

--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import { Link, useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData, useNavigate } from 'react-router-dom';
 
 const PostDetails = () => {
     const post = useLoaderData();
+    const navigate = useNavigate();
     const {id, body, title} = post;
+
+    const handleGoBack = () => {
+        // navigate(-1);
+        navigate('/posts')
+    }
     return (
         <div>
             <h2>Post Id: {id}</h2>
             <h2>Post title: {title}</h2>
             <p>{body}</p>
-            <Link to = "/posts">Back</Link>
+            {/* <Link to = "/posts">Back</Link> */}
+            <button onClick={handleGoBack}> Go Back </button>
         </div>
     );
 };
